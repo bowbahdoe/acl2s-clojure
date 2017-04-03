79c6f82ee125b5b86d43419d4c26af7f9bc709a2
Ethan McCue <emccue@live.com>
2017-04-03 15:09:31 -0400

    added method stubs and some tests for std. lib

--- src/acl2s/core.clj
+++ src/acl2s/core.clj
@@ -1,5 +1,7 @@
 (ns acl2s.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.pprint :refer [pprint]]
+            [clojure.test :refer [is]]))
 ;; Macros to support placing definitions
 ;; proved to be correct in ACL2S begginer mode into a clojure program
 
@@ -8,23 +10,89 @@
 ;; -- Number
 ;; -- |List ACL2SCode|
 
+;; Any -> Boolean
+;; returns if l is a list
+(defn listp [l]
+  (or (list? l) (nil? l)))
+
+;; ----------------------------------------------------------------------------
+;; Any -> Boolean
+;; returns if l is a cons
+(defn consp [l]
+  (and (listp l) (not (empty? l))))
+
+;; ----------------------------------------------------------------------------
+;; Any -> Boolean
+;; returns if l is an empty list
+(defn endp [l]
+  (or (= l nil) (and (listp l) (empty? l))))
+
+;; ----------------------------------------------------------------------------
+;; |List x| -> |List x|
+;; reverses the given list
+(defn rev [l]
+  (reverse l))
+
+;; ----------------------------------------------------------------------------
+;; |List x|, |List y| -> |List |Union x y||
+;; Appends list a onto list b
+(defn app [a b]
+  (concat a b))
+
+;; ----------------------------------------------------------------------------
+;; Any, Any -> Boolean
+;; returns if two things are equal
+(defn equal [a b]
+  (= a b))
+
+;; ----------------------------------------------------------------------------
+;; Any -> Boolean
+;; returns if i is an integer
+(defn integerp [i]
+  (integer? i))
+
+;; ----------------------------------------------------------------------------
+;; Any -> Boolean
+;; returns if n is a natural number
+(defn natp [n]
+  (and (integerp n) (>= n 0)))
+
+;; ----------------------------------------------------------------------------
+;; Any -> boolean
+;; returns if n is a positive natural number
+(defn posp [n]
+  (and (pos? n) (integerp n)))
+
+;; ----------------------------------------------------------------------------
+;; Any -> Boolean
+;; returns if b is a boolean
+(defn booleanp [b]
+  (or (nil? b) (= b true) (= b false)))
+
+;; ----------------------------------------------------------------------------
+;; Boolean, Boolean -> Boolean
+;; gives logical a => b
+(defmacro implies [a b]
+  (if a b true))
+
 (defonce acl2s-substitutions
-  '((listp list?)
-    (consp cons?)
-    (endp empty?)
-    (rev reverse)
-    (app concat)
-    (equal =)
-    (integerp integer?)
-    (natp (fn [n] (or (= n 0)
-                      (and (pos? n) (integer? n)))))
-    (posp (fn [n] (and (pos? n) (integer? n))))
-    (integerp integer?)
-    (t true)
+  (list
+    (list 'listp `listp)
+    (list 'consp `consp)
+    (list 'endp `endp)
+    (list 'rev `rev)
+    (list 'app `app)
+    (list 'equal `equal)
+    (list 'integerp `integerp)
+    (list 'natp `natp)
+    (list 'posp `posp)
+    (list 'booleanp `booleanp)
+    (list 'implies `implies)
+    (list 't true)
     ))
 
 ;; ----------------------------------------------------------------------------
-;; Symbol, |List |List Symbol Symbol|| -> Symbol
+;; Symbol, |List |List Symbol |Union Symbol Function||| -> Symbol
 ;; Takes in a symbol and a list of possible replacements
 ;; for that symbol. If that symbol is in the replacements
 ;; list, returns the replacement, else returns the symbol
@@ -47,25 +115,47 @@
           (map make-replacements code-body)
          :else code-body))
 
+(defn convert-cond [code-body] code-body)
+(defn convert-let [code-body] code-body)
+
+(defn fix-functions [code-body]
+  (convert-cond (convert-let code-body)))
+
 (defmacro defunc [function-name arguments input-contract
                   input-test output-contract output-test body]
   (list 'defn function-name (into [] arguments)
-        (list 'if (make-replacements input-test)
-                  (make-replacements body)
-                  '(throw (Exception. "Input contract failed")))))
+        (fix-functions (make-replacements body))))
+
+(defmacro test? [expr] nil)
+(defmacro defthm [&stuff] nil)
+(defmacro thm [&stuff] nil)
+(defmacro defdata [name definition] nil)
+(defmacro check= [a b] nil)
 
 (defunc fib (n)
   :input-contract (natp n)
   :output-contract (natp (fib n))
+  (if (or (equal n 0) (equal n 1))
+      n
+      (+ (fib (- n 1))
+         (fib (- n 2)))))
+
+(defunc fib-t (a b n)
+  :input-contract (and (natp a) (natp b) (natp n))
+  :output-contract (natp (fib-t a b n))
   (if (equal n 0)
-      0
-      (if (equal n 1)
-          1
-          (+ (fib (- n 1))
-             (fib (- n 2))))))
+      b
+      (fib-t (+ a b) a (- n 1))))
+
+(defunc fib* (n)
+  :input-contract (natp n)
+  :output-contract (natp (fib* n))
+  (fib-t 1 0 n))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println (macroexpand '(a 1 2)))
-  (println (fib 10)))
+  (println (fib 10))
+  (println (fib 20))
+  (println (fib 30))
+  (println (fib 40))
+  (println (fib* 50)))
--- test/acl2s/core_test.clj
+++ test/acl2s/core_test.clj
@@ -2,6 +2,47 @@
   (:require [clojure.test :refer :all]
             [acl2s.core :refer :all]))
 
+(deftest acl2s-std-lib-test
+  (testing "listp tests"
+           (is (listp nil))
+           (is (listp '()))
+           (is (listp '(1 2 a b)))
+           (is (listp (list 'a 'b 1 2)))
+           (is (not (listp 2)))
+           (is (not (listp 'a)))
+           (is (not (listp [])))
+           (is (not (listp [1 2 3])))
+           (is (not (listp {})))
+           (is (not (listp {:key 'value})))
+           (is (not (listp :item)))
+           (is (not (listp #{'set 'of 'items}))))
+  (testing "consp tests"
+           (is (not (consp nil)))
+           (is (not (consp '())))
+           (is (consp '(1 2 a b)))
+           (is (consp (list 'a 'b 1 2)))
+           (is (not (consp 2)))
+           (is (not (consp 'a)))
+           (is (not (consp [])))
+           (is (not (consp [1 2 3])))
+           (is (not (consp {})))
+           (is (not (consp {:key 'value})))
+           (is (not (consp :item)))
+           (is (not (consp #{'set 'of 'items}))))
+  (testing "endp tests"
+           (is (endp nil))
+           (is (endp '()))
+           (is (not (endp '(1 2 a b))))
+           (is (not (endp (list 'a 'b 1 2))))
+           (is (not (endp 2)))
+           (is (not (endp 'a)))
+           (is (not (endp [])))
+           (is (not (endp [1 2 3])))
+           (is (not (endp {})))
+           (is (not (endp {:key 'value})))
+           (is (not (endp :item)))
+           (is (not (endp #{'set 'of 'items})))))
+
 (deftest symbol-replacement-test
   (testing "Symbol replacement"
            (is (= (replace-symb 'a '((a b))) 'b))
@@ -9,19 +50,3 @@
            (is (= (replace-symb 'a '()) 'a))
            (is (= (replace-symb 'a '((b a) (a c))) 'c))))
 
-
-
-(def acl2s-fib
-  '(cond ((equal n 0) 0)
-          ((equal n 1) 1)
-          (t (+ (fib (- n 1))
-                (fib (- n 2))))))
-(def clojure-fib
-  '(cond ((= n 0) 0)
-          ((= n 1) 1)
-          (true (+ (fib (- n 1))
-                (fib (- n 2))))))
-
-(deftest make-replacements-test
-  (testing "Making function name replacements"
-           (is (= (make-replacements acl2s-fib) clojure-fib))))
